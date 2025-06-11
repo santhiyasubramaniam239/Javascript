3785358bdde4eff9aad49588134444f66f41f645
santhiyasubramaniam239 <santhiyasubramaniam239@gmail.com>
2025-06-11 16:08:00 +0530

    string methods

--- practice.js
+++ practice.js
@@ -86,7 +86,7 @@ for (let val of a) {
     console.log(val);
 }*/
 /* Array Methods*/
-let a = ["HTML", "CSS", "JS"];
+/*let a = ["HTML", "CSS", "JS"];
 console.log(a.length);
 let s = a.toString();
 console.log(s);
@@ -145,4 +145,76 @@ mat[1][2] = 100;
 console.log(mat);
 a.forEach(function myfunc(x) {
     console.log(x);
-});
+});*/
+/* object methods */
+
+/*let student = {
+    name: "Martin",
+    class: "12th",
+    Grade: "A",
+
+    studentDetails: function () {
+        return this.name + " " + this.class
+            + " " + this.Grade + " ";
+    }
+};
+console.log(student.studentDetails());
+const person2 = {firstName: "Anne",lastName: "Smith"};
+Object.assign(student, person2);
+let text = Object.entries(student);
+console.log(text);
+const fruits = [
+["apples", 300],
+["pears", 900],
+["bananas", 500]
+];
+const myObj = Object.fromEntries(fruits);
+console.log(myObj.pears);
+let keys = Object.keys(student);
+console.log(keys);
+let values = Object.values(student);
+console.log(values);
+const fruits1 = [
+  {name:"apples", quantity:300},
+  {name:"bananas", quantity:500},
+  {name:"oranges", quantity:200},
+  {name:"kiwi", quantity:550}
+];
+
+// Callback function to Group Elements
+function myCallback({ quantity }) {
+  return quantity > 300 ? "ok" : "low";
+}
+
+// Group by Quantity
+const result = Object.groupBy(fruits1, myCallback);
+console.log(result);*/
+
+/* String Methods*/
+let A = 'Welcome to my world';
+let b = A.slice(14, 19);
+console.log(b);
+let c = A.substring(0, 7);
+console.log(c);
+let d = A.substr(11, 19);
+console.log(d);
+let e = A.replace("my", "new");
+console.log(e);
+let e1 = "Mind, Alpha, Alpha, Soul";
+let e2 = e1.replaceAll("Alpha", "Space");
+console.log(e2);
+console.log(A.toUpperCase());
+console.log(A.toLowerCase());
+console.log(e.concat(A));
+let f = "   Home   "
+console.log(f.trim());
+console.log(f.trimStart());
+console.log(f.trimEnd());
+let g = f.trim();
+console.log(g.length);
+console.log(g.padStart(10, "Sweet "));
+console.log(g.padEnd(10, "Sweet "));
+console.log(g.charAt(1));
+console.log(g.charCodeAt(1));
+let g1 = 'HTML-stands-for-HyperTextMarkupLanguage'
+console.log(g1.split('-'));
